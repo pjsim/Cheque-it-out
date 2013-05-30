e670381c0e7fce8ad17a967fce8d5e37e9a9565f
pjsim <phillip.j.simmonds@gmail.com>
2013-05-30 19:25:27 +1000

    final

--- app/controllers/cheques_controller.rb
+++ app/controllers/cheques_controller.rb
@@ -4,14 +4,18 @@ class ChequesController < ApplicationController
   def index
     if params[:drawer]
       @cheques = Cheque.where(:drawer => params[:drawer])
+
     elsif params[:payee]
       @cheques = Cheque.where(:payee => params[:payee])
+
     elsif params[:drawer_payee]
       @cheques_drawer = Cheque.where(:drawer => params[:drawer_payee])
       @cheques_payee = Cheque.where(:payee => params[:drawer_payee])
       @cheques = @cheques_drawer + @cheques_payee
+
     else
       @cheques = Cheque.all
+      
     end
 
     respond_to do |format|
